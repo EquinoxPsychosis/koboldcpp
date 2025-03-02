430e8fa5d6305f156bc24f6d910e5e7a2a3d6e7f
CasualAutopsy <casual_autopsy@outlook.com>
2025-03-02 14:06:48 -0500

    Add Truncation Sampler to Top N Sigma

--- gpttype_adapter.cpp
+++ gpttype_adapter.cpp
@@ -1722,7 +1722,36 @@ const std::vector<samplers> & sampler_order, llama_grammar * grammar, float dyna
     }
     else if (nsigma > 0.0f)
     {
-        sample_top_k(&candidates_p, top_k);
+        for (int i = 0; i < sampler_order.size(); i++) {
+            switch (sampler_order[i]) {
+                case KCPP_SAMPLER_TOP_K:
+                    sample_top_k(&candidates_p, top_k);
+                    break;
+                case KCPP_SAMPLER_TOP_A:
+                    break;
+                case KCPP_SAMPLER_TOP_P:
+                    sample_min_p(&candidates_p, min_p, 1);
+                    break;
+                case KCPP_SAMPLER_TFS:
+                    break;
+                case KCPP_SAMPLER_TYP:
+                    break;
+                case KCPP_SAMPLER_TEMP:
+                    break;
+                case KCPP_SAMPLER_SMOOTH:
+                    break;
+                case KCPP_SAMPLER_REP_PEN:
+                    break;
+                case KCPP_SAMPLER_PRES_PEN:
+                    break;
+                case KCPP_SAMPLER_OCCR_PEN:
+                    break;
+                default:
+                    printf("\nSampleLogits: Unknown Sampler : %d", sampler_order[i]);
+                    break;
+            }
+        }
+
         if (dynatemp_range != 0) {
             float dynatemp_min = temp - dynatemp_range;
             float dynatemp_max = temp + dynatemp_range;
@@ -1743,12 +1772,16 @@ const std::vector<samplers> & sampler_order, llama_grammar * grammar, float dyna
                 case KCPP_SAMPLER_TOP_K:
                     break;
                 case KCPP_SAMPLER_TOP_A:
+                    sample_top_a(&candidates_p, top_a, 1);
                     break;
                 case KCPP_SAMPLER_TOP_P:
+                    sample_top_p(&candidates_p, top_p, 1);
                     break;
                 case KCPP_SAMPLER_TFS:
+                    sample_tail_free(&candidates_p, tfs, 1);
                     break;
                 case KCPP_SAMPLER_TYP:
+                    sampler_typical(&candidates_p, typical_p, 1)
                     break;
                 case KCPP_SAMPLER_TEMP:
                     break;
