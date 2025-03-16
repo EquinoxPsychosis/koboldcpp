66350d26db007bbd89f840db981ebdc4a582e67d
CasualAutopsy <casual_autopsy@outlook.com>
2025-03-16 08:42:26 -0400

    Revert Version Change

--- koboldcpp.py
+++ koboldcpp.py
@@ -49,7 +49,7 @@ logit_bias_max = 512
 dry_seq_break_max = 128
 
 # global vars
-KcppVersion = "s-beta k-1.85"
+KcppVersion = "1.85"
 showdebug = True
 kcpp_instance = None #global running instance
 global_memory = {"tunnel_url": "", "restart_target":"", "input_to_exit":False, "load_complete":False}
