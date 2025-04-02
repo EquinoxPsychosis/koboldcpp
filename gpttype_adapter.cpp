491adf7cae37c2298d58af6ed3073faa470a4850
CasualAutopsy <casual_autopsy@outlook.com>
2025-04-02 11:27:40 -0400

    Min P Changes Pt.3

--- gpttype_adapter.cpp
+++ gpttype_adapter.cpp
@@ -1382,7 +1382,7 @@ void sample_min_p(llama_token_data_array * cur_p, float p, size_t min_keep, size
     bool min_p_applied = false;
 
     // if the cur_p aren't sorted, try the unsorted implementation first
-    if (!cur_p->sorted && nsigma <= 0.0f) {
+    if (!cur_p->sorted && ((top_k <= 0 || top_k > 200) && nsigma > 0.0f)) {
         std::vector<llama_token_data> filtered_tokens;
 
         float max_logit = -FLT_MAX;
@@ -1425,7 +1425,7 @@ void sample_min_p(llama_token_data_array * cur_p, float p, size_t min_keep, size
         }
 
         // Resize the output vector to keep only the matching tokens
-        if ((top_k > 1 && top_k < 201) && nsigma > 0.0f) {
+        if ((top_k > 0 && top_k <= 200) && nsigma > 0.0f) {
             cur_p->size = i + top_k;
         } else {
             cur_p->size = i;
